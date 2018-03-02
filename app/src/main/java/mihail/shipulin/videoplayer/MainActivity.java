c161cdac31f1fb10af7339cf51caefec3c0a78f5
fuckstrot <sibilare@gmail.com>
2018-03-02 17:51:27 +0300

    volley dependency added

--- app/src/main/java/mihail/shipulin/videoplayer/MainActivity.java
+++ app/src/main/java/mihail/shipulin/videoplayer/MainActivity.java
@@ -1,28 +1,32 @@
 package mihail.shipulin.videoplayer;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Environment;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.MediaController;
-import android.widget.Toast;
 import android.widget.VideoView;
 
+import mihail.shipulin.videoplayer.services.BackGroundService;
+
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
     VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Log.d(TAG, "requestWindowFeature(Window.FEATURE_NO_TITLE)");
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Log.d(TAG, "setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)");
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"createBackGroundService()" );
+        Intent intent = new Intent(this, BackGroundService.class);
+        startService(intent);
         videoView = (VideoView)findViewById(R.id.videoView1);
         //Creating MediaController
         //MediaController mediaController= new MediaController(this);
@@ -33,14 +37,17 @@ public class MainActivity extends Activity {
         //videoView.setMediaController(mediaController);
         videoView.setKeepScreenOn(true);
         //videoView.setVideoURI(uri);
+        Log.d(TAG,"android.resource://" + getPackageName() + "/" + R.raw.big_buck_bunny );
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.big_buck_bunny);
         videoView.requestFocus();
+        Log.d(TAG,"videoView.requestFocus()" );
         videoView.start();
+        Log.d(TAG,"videoView.start()" );
     }
-
 
     @Override
     protected void onResume() {
+        Log.d(TAG,"onResume()" );
         super.onResume();
         videoView.requestFocus();
         videoView.start();
@@ -48,18 +55,9 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onPause() {
+        Log.d(TAG,"onPause()" );
         super.onPause();
         videoView.pause();
     }
 
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        // Checks the orientation of the screen
-//        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-//            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-//        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
